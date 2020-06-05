d9ec6f90c42b3031a0504c22c5c1ce983a2efb38
shryzhang <996707719@qq.com>
2020-06-05 10:26:26 +0800

    refactor: 代码规范配置强化

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 function helloWorld() {
-  console.log(
-    '欢迎使用，开始进行原生js组件开发吧~^_^产长产长长长海沧产长产长长长海沧产长产长长长海沧产长产长长长海沧产长产长长长海沧'
-  );
-}
-
-function test() {
-  console.log('test');
+  console.log('欢迎使用，开始进行原生js组件开发吧~^_^');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
-const TerserWebpackPlugin = require("terser-webpack-plugin");
+const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: "none",
+  mode: 'none',
   entry: {
-    "my-component": "./src/index.js",
-    "my-component.min": "./src/index.js",
+    'my-component': './src/index.js',
+    'my-component.min': './src/index.js',
   },
   output: {
-    filename: "[name].js",
-    library: "myComponent",
-    libraryTarget: "umd",
-    libraryExport: "default",
+    filename: '[name].js',
+    library: 'myComponent',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
   },
   optimization: {
     minimize: true,
